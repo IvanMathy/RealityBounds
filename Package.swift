8fc035061356ede020ff6f6ad22aeb9890c992ff
Ivan Mathy <IvanMathy@users.noreply.github.com>
2023-09-07 17:34:28 -0700

    Added platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RealityBounds",
+    platforms: [.iOS(.v15), .macOS(.v13), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
